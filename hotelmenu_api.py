578fdf72d57b6589f7c464e2376a35ecc5a5c264
Dhiraj Patil <75991425+dhiraj-patil-analyst@users.noreply.github.com>
2024-06-08 21:05:22 +0530

    Update hotelmenu_api.py

--- hotelmenu_api.py
+++ hotelmenu_api.py
@@ -1,67 +1,59 @@
 from flask import Flask,request
 app=Flask(__name__)
-items=[{
-       "Name":"Misalpav",
-        "Price":20
-       },
-       {"Name":"Samosa",
-       "Price":20},
-       {"Name":"Biryani","Price":140}
+
+items=[
+    {'Name':'Puranpoli','Price':150},
+{'Name':'Puribhaji','Price':80},
+{'Name':'Misal','Price':120}
 ]
 
 
 @app.get('/get-items')
-def get_all_items():
-  return {"Items":items}
+def get_items():
+    return {"Items":items}
 
-# for dynamic url
-
-''' @app.get('/get-item/<string:name>')
-def get_item (name):
-    for item in items:
-         if name == item['Name']:
-             return item
-    return {'message':"Record doesn't exist"}    '''
-
-# For key value addition method
-
-          
-@app.get('/get-item')
-def get_item ():
-    Name=request.args.get('Name')
-    for item in items:
-         if Name == item['Name']:
-             return item
-    return {'message':"Record doesn't exist"} ,404
-
-@app.post('/add-items')
+@app.post('/add-item')
 def add_item():
     request_data=request.get_json()
     items.append(request_data)
-    return {"message":"Item added successfully!"},201
+    return {"Message":"Item added succesfully!"},201
+'''
+# Dynamic url
+@app.get('/get-item/<string:name>')
+def get_item(name):
+    for item in items:
+        if name==item["Name"]:
+            return item
+    return {"Message":"Record doesn't exist"}    
+
+'''
+@app.get('/get-item')
+def get_item():
+    name=request.args.get('name')
+    for item in items:
+        if item["Name"] ==name:
+            return item
+    return {"Message":"Record doesn't exist"} 
 
 @app.put('/update-item')
 def update_item():
     request_data=request.get_json()
     for item in items:
-        if item['name']==request_data['name']:
-            item['price']=request_data['price']
-            return {"message":"Item updated successfully!"}
-    return {'Message':"Record doesn't exist"},404
+        if item['Name']==request_data['name']:
+            item["Price"]=request_data["price"]
+            return {"Message":"Item updated successfully!"}
+    return {"Message":"Record doesn't exist"},404
+
+@app.delete('/delete-item')
+def delete_item():
+    name=request.args.get('name')
+    for item in items:
+        if item["Name"]==name:
+            items.remove(item)
+            return {"Message":"Item deleted successfully!"}
+    return {"Message":"Record doesn't exist"} 
 
 
 
 if __name__=="__main__":
   app.run(debug=True)
-
-
-#abcd
-  
-  
-  
-  
-  
-  
-  
-  
-  
