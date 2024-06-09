ab3dfef211d4fdeb139617210daadb0f4df962d6
Dhiraj Patil <75991425+dhiraj-patil-analyst@users.noreply.github.com>
2024-06-09 22:54:51 +0530

    Create db.py

---
+++ db.py
@@ -0,0 +1,5 @@
+items={
+    "77561fe58fce4a0abebf2e95def6af2f":{'Name':'Puranpoli','Price':150},
+"8b90b15eb89a43c9a141f4f99bcf7bbc":{'Name':'Puribhaji','Price':80},
+"9f87146f0dcc441690a3750fdb6b1197":{'Name':'Misal','Price':120}
+}
